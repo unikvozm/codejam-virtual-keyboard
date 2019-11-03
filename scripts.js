338caa50aa458d3c721ae8528db6f90503383263
Natalia Volkova <natalia.volkofff@gmail.com>
2019-11-03 12:38:01 +0300

    feat: add changeLayout function on shift-click

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const layout = {
   'rus': {
 		'default': [['ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'], 
-								['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
+								['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
 								['Caps', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
 								['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '∧', 'Shift'],
 								['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl']
 							],
 		'shift': [['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'], 
-							['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/'],
+							['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del'],
 							['Caps', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
 							['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '∧', 'Shift'],
 							['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl']
@@ -16,25 +16,40 @@ const layout = {
 	},
 	'eng': {
 		'default': [['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'], 
-								['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-								['Caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
+								['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
+								['Caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
 								['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '∧', 'Shift'],
 								['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl']
 							],
 		'shift': [['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'], 
-							['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'],
+							['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del'],
 							['Caps', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'],
 							['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '∧', 'Shift'],
 							['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '∨', '>', 'Ctrl']
 						],
-
 	}
 };
 
+const keysCode = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+									'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
+									'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+									'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+									'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'
+								];
+
+const keysNum = [
+									layout.rus.default[0].length, 
+									layout.rus.default[1].length, 
+									layout.rus.default[2].length, 
+									layout.rus.default[3].length, 
+									layout.rus.default[4].length
+								];
+
 class Keyboard {
 	constructor (language, isShifted) {
+		this.language = language;
 		this.option = isShifted ? 'shift' : 'default';
-		this.layout = [...layout[language][this.option]];
+		this.layout = [...layout[this.language][this.option]];
 		this.keyboard = document.createElement('div');
 		this.keyboard.className = 'keyboard';
 	}
@@ -54,12 +69,65 @@ class Keyboard {
 		document.body.append(this.keyboard);
 		return;
 	}
+
+	shift() {
+		this.isShifted = !this.isShifted;
+		this.changeLayout(this.language, this.isShifted);
+		return;
+	}
+
+	language() {
+
+	}
+
+	changeLayout(language, isShifted) {
+		this.option = isShifted ? 'shift' : 'default';
+		this.language = language;
+		this.layout = [...layout[this.language][this.option]];
+		let keys = document.querySelectorAll('.key');
+		keys.forEach((key, i) => {
+			if (i < keysNum[0])	key.innerHTML = this.layout[0][i];
+			else if (i < keysNum[0] + keysNum[1]) {
+				key.innerHTML = this.layout[1][i - keysNum[0]];
+			} else if (i < keysNum[0] + keysNum[1] + keysNum[2]) {
+				key.innerHTML = this.layout[2][i - keysNum[0] - keysNum[1]];
+			}	else if (i < keysNum[0] + keysNum[1] + keysNum[2] + keysNum[3]) {
+				key.innerHTML = this.layout[3][i - keysNum[0] - keysNum[1] - keysNum[2]];
+			} else {
+				key.innerHTML = this.layout[4][i - keysNum[0] - keysNum[1] - keysNum[2] - keysNum[3]];
+			}
+		});
+		return;
+	}
 }
+
+let language;
+if (localStorage.getItem('language') === null) {
+	language = navigator.language.inclused('ru') ? 'rus' : 'eng';
+	localStorage.setItem('language', language);
+}
+else language = localStorage.getItem('language');
+let isShifted = false;
+let keyboard = new Keyboard(language, isShifted);
 
 window.onload = function() {
-	let language = 'rus';
-	let isShifted = false;
-	let keyboard = new Keyboard(language, isShifted);
+	document.body.append(document.createElement('TEXTAREA'));
+	let language;
+	if (window.localStorage.getItem('language') === null) {
+		language = navigator.language.includes('ru') ? 'rus' : 'eng';
+		window.localStorage.setItem('language', language);
+	}
+	else language = localStorage.getItem('language');
 	keyboard.createLayout();
-
 }
+
+document.body.addEventListener('click', function(event) {
+	if (event.target.classList.contains('key')) {
+		if (event.target.innerHTML === 'Shift') {
+			event.target.classList.toggle('active');
+			keyboard.shift();
+		}
+	}
+});
+
+
